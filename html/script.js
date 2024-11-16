4cd381aed4c357b16cf08ce4fa7b61c9e2bb6ffa
Abdelrmb <richeabdelrahim1@gmail.com>
2024-11-16 12:15:36 +0100

    again

--- html/script.js
+++ html/script.js
@@ -27,6 +27,6 @@ function showNotification(message, type, duration) {
 
 function playSound() {
     const audio = new Audio('notification.mp3');
-    audio.volume = 0.2;
+    audio.volume = 0.05;
     audio.play();
 }
